3434f6313e1c2796bbc3242c3ae7389f6029bc51
jedrekszor <jedrekszor@wp.pl>
2018-12-02 00:01:38 +0100

    killing animation

--- Assets/Scripts/MarkerBounce.cs
+++ Assets/Scripts/MarkerBounce.cs
@@ -12,7 +12,7 @@ public class MarkerBounce : MonoBehaviour
 	
 	// Use this for initialization
 	void Start () {
-		player = GameObject.Find("Cultist 1");
+		player = GameObject.Find("Cultist");
 		init = transform.position.z - player.transform.position.z;
 
 	}
