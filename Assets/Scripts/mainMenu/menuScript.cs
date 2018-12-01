d4b0f4bfbe7a2bb1966206a44a6dbe4b1cfd0975
antek2137 <antonio25698@gmail.com>
2018-12-01 16:39:46 +0100

    menu podejscie 2

---
+++ Assets/Scripts/mainMenu/menuScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuScript : MonoBehaviour {
+
+	public void PlayGame()
+	{
+		SceneManager.LoadScene("game_temp");
+	}
+
+	public void QuitGame()
+	{
+		Debug.Log("Quit!");
+		Application.Quit();
+	}
+}
