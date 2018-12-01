c4e812e1b57ed05b401d99eb56c03435c3459bb4
jedrekszor <jedrekszor@wp.pl>
2018-12-01 17:12:07 +0100

    music

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -2,15 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicManager : MonoBehaviour {
+public class MusicManager : MonoBehaviour
+{
+	private AudioSource source;
 
+	[SerializeField] private AudioClip _satan;
+	[SerializeField] private AudioClip _noSatan;
+	private bool _satanist;
+	
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+		source = gameObject.GetComponent<AudioSource>();
+		source.clip = _noSatan;
+		_satanist = false;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		if (Satan.active && !_satanist)
+		{
+			source.clip = _satan;
+			source.Play();
+			_satanist = true;
+		}
+		else if (!Satan.active && _satanist)
+		{
+			source.clip = _noSatan;
+			source.Play();
+			_satanist = false;
+		}
 	}
 }
