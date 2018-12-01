2b2dd5dae95741b8acd97d5b46db201198d7f79e
jedrekszor <jedrekszor@wp.pl>
2018-12-01 11:35:28 +0100

    killing piglets is now possible

--- Assets/Scripts/Cultist.cs
+++ Assets/Scripts/Cultist.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Cultist : MonoBehaviour {
@@ -7,13 +8,28 @@ public class Cultist : MonoBehaviour {
 	[SerializeField] private Sprite piglet;
 	[SerializeField] private Sprite cultist;
 	[SerializeField] private float vel;
-	[SerializeField] private TextMesh _text;
+	[SerializeField] private TextMeshProUGUI _text;
 
 	private string letters = "abcdefghijklmnopqrstuvwyz";
 	private bool flag = true;
-	private char[] result;
-	
-	
+	private KeyCode[] _keys = new KeyCode[5];
+	private int _currKey;
+	private Piglet _touchedPiglet;
+
+	[SerializeField]
+	private bool _isBeast = false;
+	public bool IsBeast
+	{
+		get { return _isBeast; }
+		set
+		{
+			_isBeast = value;
+			if (value)
+			{
+				_currKey = 0;
+			}
+		}
+	}
 	
 	void Start () {
 		gameObject.GetComponent<SpriteRenderer>().sprite = piglet;
@@ -22,7 +38,25 @@ public class Cultist : MonoBehaviour {
 	void Update ()
 	{
 		if (Satan.active)
+		{
 			gameObject.GetComponent<SpriteRenderer>().sprite = cultist;
+			if (_currKey <= 4)
+			{
+				if (Input.GetKeyDown(_keys[_currKey]))
+				{
+					_currKey++;
+
+					if (_currKey == 4)
+					{
+						_touchedPiglet.gameObject.SetActive(false);
+						_text.text = "";
+						flag = true;
+						_currKey = 0;
+
+					}
+				}
+			}
+		}
 		else
 			gameObject.GetComponent<SpriteRenderer>().sprite = piglet;
 
@@ -48,31 +82,32 @@ public class Cultist : MonoBehaviour {
 			gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, gameObject.GetComponent<Rigidbody2D>().velocity.y);
 	}
 
-	private void OnCollisionStay2D(Collision2D collision)
+	private void OnCollisionEnter2D(Collision2D collision)
 	{
 		if (collision.gameObject.CompareTag("piglet"))
 		{
 			if (Satan.active && flag)
+			{
+				_touchedPiglet = collision.gameObject.GetComponent<Piglet>();
 				purge();
+			}
 		}
 	}
 
 	void purge()
 	{
-//		for (int i = 0; i < 5;i++)
-//		{
-			//_text.text += " " + Rand();
-		
-				
-			//result[i] = letters[Random.Range(0, letters.Length - 1)];
-			//Debug.Log(Rand());
-		//}
-		//Destroy(gameObject);
+		for (int i = 0; i < 5;i++)
+		{
+			string key = Rand();
+			_text.text += " " + key;
+			_keys[i] = (KeyCode) System.Enum.Parse(typeof(KeyCode), key.ToUpper());
+		}
 		flag = false;
+		
 	}
 
-	char Rand()
+	string Rand()
 	{
-		return letters[Random.Range(0, letters.Length)];
+		return letters[Random.Range(0, letters.Length)].ToString();
 	}
 }
