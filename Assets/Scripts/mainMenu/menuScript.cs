c74b5dab185d1433a48837482a76083e09d38773
antek2137 <antonio25698@gmail.com>
2018-12-02 13:38:39 +0100

    menuuu

--- Assets/Scripts/mainMenu/menuScript.cs
+++ Assets/Scripts/mainMenu/menuScript.cs
@@ -2,12 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR.WSA.Input;
+using ADBannerView = UnityEngine.iOS.ADBannerView;
 
-public class menuScript : MonoBehaviour {
+
+public class menuScript : MonoBehaviour
+{
+	[SerializeField] private AudioClip SatanLaugh;
+	[SerializeField] private AudioClip Stab;
+	
+	[SerializeField] private AudioSource source;
+
+	IEnumerator Sleep(float time)
+	{
+		yield return new WaitForSeconds(time);
+		SceneManager.LoadScene("game_temp");
+		Debug.Log("game_temp");
+	}
+
+	private void Start()
+	{
+		source = gameObject.GetComponent<AudioSource>();
+	}
+
+	public void PlayStab()
+	{
+		source.clip = Stab;
+		source.PlayOneShot(Stab);
+	}
 
 	public void PlayGame()
 	{
-		SceneManager.LoadScene("game_temp");
+		source.clip = SatanLaugh;
+		source.PlayOneShot(SatanLaugh);
+
+		StartCoroutine(Sleep(6));
 	}
 
 	public void QuitGame()
